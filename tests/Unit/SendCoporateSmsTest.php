5687c5f25e13958d0affa987d0884d2c2b9ceb6f
3m1n3nc3 <mygames.ng@gmail.com>
2024-02-05 11:31:02 +0100

    Fix

--- tests/Unit/SendCoporateSmsTest.php
+++ tests/Unit/SendCoporateSmsTest.php
@@ -44,10 +44,9 @@ test('Throws Invalid SenderId', function () {
     loadEnv();
 
     $numbers = explode(',', $_ENV['TEST_NUMBERS'] ?? $_ENV['TEST_NUMBERS'] ?? '');
-    $otp = rand(111111, 999999);
 
     if (isset($numbers[0])) {
-        expect(fn () => (new SmsSender('xxx'))->corporate()->send($numbers[0], $otp, 'TonEfLiX', '1111'))
+        expect(fn () => (new SmsSender('xxx'))->corporate()->send($numbers[0], 'This is a test SMS.'))
         ->toThrow(SmsSendingException::class, 'Unable to send sms: The sender ID do not exist.');
     } else {
         expect(false)->toBeTrue();
